503d9b509c0f53695f79cee9c85fedc5dadc8e7b
harrisonlim <harrison.lim.hx@gmail.com>
2016-01-18 10:36:41 +0000

    added test for unique_tag, added it to index view to display author's initials

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
+
+	test "unique_tag" do
+		quote  = Quote.create(author: 'Harrison Lim', saying: 'You oughta look out.')
+		expected = 'HL#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
 end
