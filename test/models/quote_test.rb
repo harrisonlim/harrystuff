4ebfab1b5dbcf47084e22715cdfe3e81d0a4afe0
harrisonlim <harrison.lim.hx@gmail.com>
2016-01-18 15:32:09 +0000

    added FactoryGirl gem, added FactoryGirl dummy test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 
 	test "unique_tag" do
-		quote  = Quote.create(author: 'Harrison Lim', saying: 'You oughta look out.')
+		quote  = FactoryGirl.create(:quote, author: 'Harrison Lim')
 		expected = 'HL#' + quote.id.to_s
 		actual = quote.unique_tag
 		assert_equal expected, actual
