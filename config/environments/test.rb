2b46c76c302cd693efd945f21dac3b8fd1cd6b73
harrisonlim <harrison.lim.hx@gmail.com>
2016-01-19 09:15:08 +0000

    added new tests to splurty app

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ Splurty::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_controller.default_url_options = {host: 'test.com'}
 end
